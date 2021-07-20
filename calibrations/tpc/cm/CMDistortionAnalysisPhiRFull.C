cc369efe584fc6096eea5f0b15dae71fef7cfc6c
Sara Kurdi <sarakurdi@Saras-MacBook-Pro.local>
2021-07-19 22:44:20 -0400

    Plotting comparison of dist to integ fluct.

--- calibrations/tpc/cm/CMDistortionAnalysisPhiRFull.C
+++ calibrations/tpc/cm/CMDistortionAnalysisPhiRFull.C
@@ -757,8 +757,9 @@ int CMDistortionAnalysisPhiRFull(int nMaxEvents = -1) {
     c1->cd(4);
     //hCMmodelSliceRvTrue->Draw("colz");
     //hSamplePerBinRZNeg->Draw("colz");
-    hCompareRTruevFluctNeg->Draw("colz");
+    //hCompareRTruevFluctNeg->Draw("colz");
     //hFluc->Draw();
+    hCompareRTruevIntFluctSmallRPos->Draw("colz");
     
     //y plots
     c2->Divide(4,1);
