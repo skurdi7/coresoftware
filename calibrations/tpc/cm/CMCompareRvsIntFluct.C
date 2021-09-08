1744949a9f79b684f7858001f860ac3bbc1ad22d
Sara Kurdi <sarakurdi@Saras-MacBook-Pro.local>
2021-09-08 18:51:09 -0400

    Added localized plots

--- calibrations/tpc/cm/CMCompareRvsIntFluct.C
+++ calibrations/tpc/cm/CMCompareRvsIntFluct.C
@@ -487,7 +487,8 @@ int CMCompareRvsIntFluct(int nMaxEvents = -1) {
 		hCompareRDiffvIntFluctDiffRPosRegion[4]->Fill(intfluctchargeDiffRPos,differenceCylPhiRPos[0]);
 
 		//localized plots
-		if((-1.0 < shifttrueCylPos[0] < 1.0) && (-82.0e6 < intfluctchargeSmallRPos < -78.0e6)){
+		///	if((-1.0 < shifttrueCylPos[0] < 1.0) && (-82.0e6 < intfluctchargeSmallRPos < -78.0e6)){
+		if((-1.0 < shifttrueCylPos[0] < 1.0)){
 		  hLocalRTruevIntFluctSmallRPosRegion4_RZ->Fill(r,zPos,1);
 		  hLocalRTruevIntFluctSmallRPosRegion4_PhiZ->Fill(phi,zPos,1);
 		}else if((-1.0 < shifttrueCylPos[0] < 1.0) && (-30.0e6 < intfluctchargeLargeRPos < -5.0e6)){
