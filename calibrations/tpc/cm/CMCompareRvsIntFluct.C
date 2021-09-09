13d5a9eec51de413d861b6942200fc287228ed27
Sara Kurdi <sarakurdi@Saras-MacBook-Pro.local>
2021-09-09 09:59:31 -0400

    Adjusting bounds of localized plots

--- calibrations/tpc/cm/CMCompareRvsIntFluct.C
+++ calibrations/tpc/cm/CMCompareRvsIntFluct.C
@@ -486,10 +486,10 @@ int CMCompareRvsIntFluct(int nMaxEvents = -1) {
 		hCompareRDiffvIntFluctLargeRPosRegion[4]->Fill(intfluctchargeLargeRPos,differenceCylPhiRPos[0]);
 		hCompareRDiffvIntFluctDiffRPosRegion[4]->Fill(intfluctchargeDiffRPos,differenceCylPhiRPos[0]);
 
-		cout << intfluctchargeSmallRPos << endl;
+		//cout << intfluctchargeSmallRPos << endl;
 		//localized plots
 		///	if((-1.0 < shifttrueCylPos[0] < 1.0) && (-82.0e6 < intfluctchargeSmallRPos < -78.0e6)){
-		if((-1.0 < shifttrueCylPos[0] < 1.0) && (-1e7 < intfluctchargeSmallRPos < 0.0)){
+		if((-1.0 < shifttrueCylPos[0] < 1.0) && (intfluctchargeSmallRPos < 0.0)){
 		  hLocalRTruevIntFluctSmallRPosRegion4_RZ->Fill(r,zPos,1);
 		}else if((-1.0 < shifttrueCylPos[0] < 1.0) && (0.0 < intfluctchargeSmallRPos < 1e7)){ //just to check that this is the only area we find int fluct values
 		  hLocalRTruevIntFluctSmallRPosRegion4_PhiZ->Fill(phi,zPos,1);
